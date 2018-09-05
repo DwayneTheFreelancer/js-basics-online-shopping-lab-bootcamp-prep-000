3597adab75c840c6fb001e3fb0d303dae0178388
Dwayne Munro <dwaynemunro1845@gmail.com>
2018-09-05 15:36:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -54,6 +54,7 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
+  tot
   if (cardNumber == "") {
     return "Sorry, we don't have a credit card on file for you.";
   }
