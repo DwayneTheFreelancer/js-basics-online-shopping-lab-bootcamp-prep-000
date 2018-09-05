fb5bf3fd149c3d5e2d34c963b72847f7bcfeb13c
Dwayne Munro <dwaynemunro1845@gmail.com>
2018-09-05 16:06:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ function placeOrder(cardNumber) {
   if (cardNumber){
     console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
   } else {
-    console.log("Sorry, we don't have a credit card on file for you.");
+    
   }
   cart = 0;
 }
