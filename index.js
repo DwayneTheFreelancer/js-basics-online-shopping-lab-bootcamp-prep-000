8254f11d16c3a486a66f9ec35c51e92f7789020e
Dwayne Munro <dwaynemunro1845@gmail.com>
2018-09-05 23:10:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -54,12 +54,13 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
-  if (cardNumber){
+  var isCardNumber = parseInt(cardNumber);
+  if (cardNumber === isCardNumber){
     console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
   } else {
     console.log("Sorry, we don't have a credit card on file for you.");
   }
   cart = [];
-  // return `Your total cost is $${total(71)}, which will be charged to the card ${placeOrder(83296759)}.`;
+  return `Your total cost is $${total(71)}, which will be charged to the card ${placeOrder(83296759)}.`;
   
 }
