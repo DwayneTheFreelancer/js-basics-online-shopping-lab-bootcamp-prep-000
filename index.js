967936634227134f0f4b366b50af58bb9c373498
Dwayne Munro <dwaynemunro1845@gmail.com>
2018-09-05 23:15:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -54,8 +54,7 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
-  var isCardNumber = parseInt(cardNumber);
-  if (cardNumber === isCardNumber){
+  if (cardNumber){
     console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
   } else {
     console.log("Sorry, we don't have a credit card on file for you.");
